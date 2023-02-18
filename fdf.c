d1f9d8076464be8cb140e5e2da92e67aad984a94
Polpi <anthotuigr@gmail.com>
2023-02-18 20:22:21 +0100

    start

--- fdf.c
+++ fdf.c
@@ -6,17 +6,48 @@
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 09:28:22 by polpi             #+#    #+#             */
-/*   Updated: 2023/02/14 11:02:39 by polpi            ###   ########.fr       */
+/*   Updated: 2023/02/18 20:22:06 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx_macos/mlx.h"
 #include <stdlib.h>
+/*
 
+	Faire une structure qui prend en charge
+		la largeur et la hauteur de la map
+	Egalement faire une structure global qui 
+		reprendra toutes les structures que nous
+		aurons pu creer afin de passer uniquement
+		celle-ci au differentes fonctions.
+	Apres avoir reussi a avoir la hauteur et la largeur
+		recuperer ligne par ligne avec GNL et spliter
+		chaque caracter afin de le stocker dans un tableau
+		d'int 
+	Le resultat devrait être un tableau d'int qui contient
+		par exemple : 	0000
+						0000
+						0000
+						0000
+		Ce qui donne un carré
+	
+	Une fois que nous avons reussir a avoir ce tableau d'int,
+		il faudra trouver un moyen de modifier les ints pour qu'ils
+		deviennent des structur contenant x,y,z ou que x,y ? 
+	
+
+
+
+*/
 int main(void)
 {
-	void	*mlx_ptr;
 
-	mlx_ptr = mlx_init();
-	free(mlx_ptr);
+
+
+
+
+	// void	*mlx_ptr;
+
+	// mlx_ptr = mlx_init();
+	// free(mlx_ptr);
 }
--- parse.c
+++ parse.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.h                                              :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: polpi <polpi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/14 09:28:19 by polpi             #+#    #+#             */
-/*   Updated: 2023/02/18 20:01:32 by polpi            ###   ########.fr       */
+/*   Created: 2023/02/18 19:49:35 by polpi             #+#    #+#             */
+/*   Updated: 2023/02/18 20:22:06 by polpi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#include "fdf.h"
 
-#include "../libft_fdf/gnl/srcs/get_next_line.h"
-#include "../libft_fdf/ft_printf/srcs/ft_printf.h"
-#include <stdlib.h>
-#include <stdio.h>
+/*Forget size x and y*/
 
-typedef struct s_vector {
-    int size_x;
-    int size_y;
-}   t_vector;
+int get_mapsize(char **map)
+{
+    int index;
+    char    **test;
 
-
-
-#endif
+    index = -1;
+    test = get_next_line(map);    
+    return (1);
+}
