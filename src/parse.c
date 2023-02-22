1de246df94b31ae26608f0470386c0a42dfce1c4
Anthony Fauchere <afaucher@c1r9s5.42lausanne.ch>
2023-02-22 15:48:26 +0100

    mapOK

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: afaucher <afaucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 19:49:35 by polpi             #+#    #+#             */
-/*   Updated: 2023/02/22 14:33:47 by afaucher         ###   ########.fr       */
+/*   Updated: 2023/02/22 15:42:25 by afaucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,41 +26,37 @@ void get_mapsize(char **av, t_env *env)
 	while (env->line[++i])
 	{
 		if (env->line[i] == ' ')
-		env->size_map.w++;
+		env->size_map.h++;
 	}
-	env->size_map.w++;
 	while (env->line)
 	{
 		env->line = get_next_line(fd);
-		env->size_map.h++;
+		env->size_map.w++;
 	}
-	printf("Line -> %s\n", env->line);
-	printf("sizeW -> %d\n", env->size_map.w);
-	printf("sizeH -> %d\n", env->size_map.h);
 	close (fd);
 }
 
 void	create_int_map(t_env *env)
 {
 	int	index;
+	int	i;
 
+	i = 0;
 	index = env->size_map.h;
-	env->map = malloc(sizeof(int) * env->size_map.w);
-	while (index != '\0')
+	env->map = malloc(sizeof(int*) * env->size_map.h);
+	while (i <= index)
 	{
-		env->map[index] = malloc(sizeof(int) * env->size_map.h);
-		index--;
+		env->map[i] = malloc(sizeof(int) * env->size_map.w);
+		i++;
 	}
 }
 
 void	add_number_in_map(t_env *env, char **av)
 {
 	int	fd;
-	int	index;
 	int	j;
 	int	i;
 
-	index = 0;
 	fd = open(av[1], O_RDONLY);
 	env->line = get_next_line(fd);
 	i = 0;
@@ -68,19 +64,28 @@ void	add_number_in_map(t_env *env, char **av)
 	{
 		env->map_char = ft_split(env->line, ' ');
 		j = -1;
-		while (env->map_char[0][++j])
-		{
-			env->map[i][j] = ft_atoi(env->map_char[0]);
-			i++;
-		}
+		while (env->map_char[++j])
+			env->map[i][j] = ft_atoi(env->map_char[j]);
+		i++;
 		env->line = get_next_line(fd);
 	}
-	printf("env_map[0][0] -> %d\n", env->map[0][0]);
+	free (env->map_char);
+	close (fd);
 }
 
 void	get_map(char **av, t_env *env)
 {
+	int	i;
+	int	j;
 	get_mapsize(av, env);
 	create_int_map(env);
 	add_number_in_map(env, av);
+	i = -1;
+	while (++i < 4)
+	{
+		printf("---------------------------\n");
+		j = -1;
+		while (++j < 3)
+			printf("env->map[i][j] -> %d\n", env->map[i][j]);
+	}
 }
